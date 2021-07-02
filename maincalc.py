16f3e8b5fc9abe2744825689f9be2b890bcd99d8
Suraj Adhikari <=>
2021-07-02 09:32:16 +0545

    mod work

--- maincalc.py
+++ maincalc.py
@@ -1,11 +1,12 @@
 
 import add_work, mul_work, sub_work, div_work
 
-a =100, b=50
+a = 100
+b= 50
 total_sum = add_work.add(100,50)
 total_sub = sub_work.sub(100,50)
-total_mul = mul_work.sub(100, 50)
-total_div = div_work.sub(100, 50)
+total_mul = mul_work.mul(100, 50)
+total_div = div_work.div(100, 50)
 
 print(f"sum = {total_sum} sub = {total_sub}")
 print(f"mul = {total_mul} div = {total_div}")
